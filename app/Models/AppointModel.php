a838989d1f963ee29e134748acab531983c9bca2
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:36:38 +0530

    Update AppointModel.php

--- app/Models/AppointModel.php
+++ app/Models/AppointModel.php
@@ -10,6 +10,7 @@ class AppointModel extends Model
     protected $table = 'tbl_appointment';
     protected $primaryKey = 'appoint_id';
     protected $allowedFields =['course_id','batch_id','lecturer_id','subject_id','appoint_data'];
+    protected $useSoftDeletes = 'true';
 
 
 }
