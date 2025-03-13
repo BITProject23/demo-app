3fd24d408a4bb6b8c64e2699b85cbe7af5ae67ef
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:36:50 +0530

    Update batchAddView.php

--- app/Views/batch/batchAddView.php
+++ app/Views/batch/batchAddView.php
@@ -36,10 +36,21 @@
                                     </div>                                                                        
                                 </div>
 
-                                <?php if(session()->has('success')):?>
+                                <?php if(session()->has('success')) : ?>
 
                                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                                         <?= session()->getFlashdata('success');?>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                <?php endif ?>
+
+                                <?php if(session()->has('errors')) : ?>
+
+                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                        <?= session()->getFlashdata('errors');?>
                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
