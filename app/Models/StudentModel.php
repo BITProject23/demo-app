6bf044830b8963a132f1aa6a2c7a5a0c898d7afc
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:36:29 +0530

    Update StudentModel.php

--- app/Models/StudentModel.php
+++ app/Models/StudentModel.php
@@ -10,7 +10,7 @@ class StudentModel extends Model
     protected $table = 'tbl_student';
     protected $primaryKey = 'student_id';
     protected $allowedFields =['student_no','student_first_name','student_last_name','student_nic','student_email',
-    'student_gender','student_bod','student_contact_no','student_address','student_status','student_registration_date'];
+    'student_gender','student_bod','student_contact_no','student_address','student_status','student_registration_date','student_qulify'];
     protected $useSoftDeletes = 'true';
 
 }
