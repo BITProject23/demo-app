0b328274139dbba1ea1b5888d075f3b6e92139b8
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:37:17 +0530

    Update courseUpdateView.php

--- app/Views/course/courseUpdateView.php
+++ app/Views/course/courseUpdateView.php
@@ -35,11 +35,27 @@
                                     </div>                                                                        
                                 </div>
 
-                                <?php if(session()->has('success')) : ?>
+                                <?php if(session()->has('success')):?>
 
-                                    <div class="alert alert-success mb-4"><?=session('success')?></div>
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                        <?= session()->getFlashdata('success');?>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
 
                                 <?php endif ?>
+
+                                <?php if(session()->has('errors')) : ?>
+
+                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                        <?= session()->getFlashdata('errors');?>
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                    </div>
+
+                                <?php endif; ?>
 
                                     
 
