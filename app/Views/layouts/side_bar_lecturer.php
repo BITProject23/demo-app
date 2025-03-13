4065de808a200350da54d01aff255d0f10b1e49e
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:37:59 +0530

    Update side_bar_lecturer.php

--- app/Views/layouts/side_bar_lecturer.php
+++ app/Views/layouts/side_bar_lecturer.php
@@ -5,7 +5,7 @@
                         <a href="#dashboard" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
                             <div class="">
                                 <i class="flaticon-computer-6 ml-3"></i>
-                                <span onclick="(function(){ window.location.href = '<?=base_url()?>/'; return false; })();return false;">Dashboard</span>
+                                <span onclick="(function(){ window.location.href = '<?=base_url()?>/Lecturer_Portal'; return false; })();return false;">Dashboard</span>
 
                             </div>
 
@@ -14,45 +14,7 @@
                         
                     </li>
 
-                    <li class="menu">
-                        <a href="#modules" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                            <div class="">
-                                <i class="flaticon-crm-screen-line "></i>
-                                <span>Lecturer Appointment</span>
-                            </div>
-                            <div>
-                                <i class="flaticon-right-arrow"></i>
-                            </div>
-                        </a>
-                        <ul class="collapse submenu list-unstyled" id="modules"  data-parent="#accordionExample">
-                            <li>
-                                <a href="<?=base_url()?>/Appointment_View">Lecturer Appintment View</a>
-                            </li>
-                        </ul>
-                    </li>
 
-                    
-                    <li class="menu">
-                        <a href="#tables" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                            <div class="">
-                                <i class="flaticon-table"></i>
-                                <span>TimeTable </span>
-                            </div>
-                            <div>
-                                <i class="flaticon-right-arrow"></i>
-                            </div>
-                        </a>
-                        <ul class="collapse submenu list-unstyled" id="tables" data-parent="#accordionExample">
-
-                            
-                            <li>
-                                <a href="<?=base_url()?>/Timetable"> View Timetable </a>
-                            </li>
-                            
-                        </ul>
-                    </li>
-                    
-                    
                     <li class="menu">
                         <a href="#charts" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
@@ -77,24 +39,57 @@
 
                         </ul>
                     </li>
-                    
 
-                    
+                    <li class="menu">
+                        <a href="#tables" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                            <div class="">
+                                <i class="flaticon-table"></i>
+                                <span>TimeTable </span>
+                            </div>
+                            <div>
+                                <i class="flaticon-right-arrow"></i>
+                            </div>
+                        </a>
+                        <ul class="collapse submenu list-unstyled" id="tables" data-parent="#accordionExample">
+
+                            
+                            <li>
+                                <a href="<?=base_url()?>/View_Timetable"> View Timetable </a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+
+                    <li class="menu">
+                        <a href="#modules" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                            <div class="">
+                                <i class="flaticon-crm-screen-line "></i>
+                                <span>Appointment</span>
+                            </div>
+                            <div>
+                                <i class="flaticon-right-arrow"></i>
+                            </div>
+                        </a>
+                        <ul class="collapse submenu list-unstyled" id="modules"  data-parent="#accordionExample">
+                            <li>
+                                <a href="<?=base_url()?>/View_Appointment">Appintment view </a>
+                            </li>
+                        </ul>
+                    </li>
+
 
                     <li class="menu">
                         <a href="#users" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
                                 <i class="flaticon-user-6"></i>
-                                <span> Profile <?= session()->get('user_name')?></span>
+                                <span> Profile - <?= session()->get('user_name')?></span>
                             </div>
                             <div>
                                 <i class="flaticon-right-arrow"></i>
                             </div>
                         </a>
                         <ul class="collapse submenu list-unstyled" id="users" data-parent="#accordionExample">
-                            <li>
-                                <a href="user_profile.html"> Profile </a>
-                            </li>
+                            
                             <li>
                                 <a href="<?=base_url()?>/User_PasswordReset"> Change Password </a>
                             </li>
