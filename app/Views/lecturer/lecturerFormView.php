711651bd66915e5b05422217a250f137d8513f5b
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:38:13 +0530

    Update lecturerFormView.php

--- app/Views/lecturer/lecturerFormView.php
+++ app/Views/lecturer/lecturerFormView.php
@@ -14,14 +14,12 @@
                 <div class="page-header">
                     <div class="page-title">
                         <h3> Lecturer 
-                            <!-- <small>Rounded</small> -->
                         </h3>
                         <div class="crumbs">
                             <ul id="breadcrumbs" class="breadcrumb">
                                 <li><a href="index-2.html"><i class="flaticon-home-fill"></i></a></li>
                                 <li><a href="#">Lecturer</a></li>
                                 <li><a href="#">Add New Lecturer</a></li>
-                                <!-- <li class="active"><a href="#">Rounded</a> </li> -->
                             </ul>
                         </div>
                     </div>
@@ -36,12 +34,24 @@
                                         <h4>Lecturer form</h4>
                                     </div>                                                                        
                                 </div>
-
-
                                 <?php if(session()->has('success')) : ?>
 
-                                    <div class="alert alert-success mb-4">
-                                        <?=session('success')?>
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                        <?= session()->getFlashdata('success');?>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                <?php endif ?>
+
+                                <?php if(session()->has('errors')) : ?>
+
+                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                        <?= session()->getFlashdata('errors');?>
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
                                     </div>
 
                                 <?php endif ?>
@@ -54,30 +64,29 @@
                             </div>
                             <div class="widget-content widget-content-area">
 
-                                <!-- < ?php echo form_open('LecCreate') ?> -->
-                                    <form name="lectureform" action="<?=base_url()?>/Lecturer_create" onsubmit="return lecturerValidateForm()" method="post">
+                                    <form name="lectureform" action="<?=base_url()?>/Lecturer_create" data-toggle="validator" method="post">
 
-                                        <div class="form-group row mb-1">
+                                        <div class="form-group row mb-4">
                                             <label for="lecturer_no" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Lecturer EmpNo </label>
                                             <div class="col-xl-10 col-lg-9 col-sm-10">
-                                                <input type="text" class="form-control-rounded form-control"  name="lecturer_no" id="lecturer_no" placeholder="">
-                                                <label id="lecturer_no_error"></label>
+                                                <input type="text" class="form-control-rounded form-control"  name="lecturer_no" id="lecturer_no" placeholder="" required 
+                                                value="<?= $lec_no?>" readonly>
                                             </div>
                                         </div>
 
-                                        <div class="form-group row mb-1">
+                                        <div class="form-group row mb-4">
                                             <label for="lecturer_first_name" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">First Name </label>
                                             <div class="col-xl-10 col-lg-9 col-sm-10">
-                                                <input type="text" class="form-control-rounded form-control"  name="lecturer_first_name" id="lecturer_first_name" placeholder="">
-                                                <label id="first_name_error"></label>
+                                                <input type="text" class="form-control-rounded form-control"  name="lecturer_first_name" id="lecturer_first_name" minlength="3" maxlength="20" placeholder="" data-required-error="Please enter a first name" required>
+                                                <div class="help-block with-errors"></div>
                                             </div>
                                         </div>
 
-                                        <div class="form-group row mb-1">
+                                        <div class="form-group row mb-4">
                                             <label for="lecturer_last_name" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Last Name </label>
                                             <div class="col-xl-10 col-lg-9 col-sm-10">
-                                                <input type="text" class="form-control-rounded form-control"  name="lecturer_last_name" id="lecturer_last_name" placeholder="">
-                                                <label id="last_name_error"></label>
+                                                <input type="text" class="form-control-rounded form-control"  name="lecturer_last_name" id="lecturer_last_name" data-required-error="Please enter a last name" required>
+                                                <div class="help-block with-errors"></div>
                                             </div>
                                         </div>
     
@@ -85,9 +94,8 @@
                                         <div class="form-group row mb-4">
                                             <label for="lecturer_nic" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">NIC </label>
                                             <div class="col-xl-10 col-lg-9 col-sm-10">
-                                                <input type="text" class="form-control-rounded form-control " name="lecturer_nic" id="lecturer_nic" placeholder="">
-                                                <!-- <span class="" id="nicmsg"> test</span> -->
-                                                <lable id="nic_error"></lable>
+                                                <input type="text" class="form-control-rounded form-control " name="lecturer_nic" id="lecturer_nic" minlength="10" maxlength="12"  data-required-error="Please enter a valid NIC number" required>
+                                                <div class="help-block with-errors"></div>
                                             </div>
                                         </div>
 
@@ -96,8 +104,8 @@
                                         <div class="form-group row mb-4">
                                             <label for="lecturer_email" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Email</label>
                                             <div class="col-xl-10 col-lg-9 col-sm-10">
-                                                <input type="email" class="form-control-rounded form-control" name="lecturer_email" id="lecturer_email" placeholder="">
-                                                <lable id="email_error"></lable>
+                                                <input type="email" class="form-control-rounded form-control" name="lecturer_email" id="lecturer_email" data-required-error="Please enter a valid email address" required>
+                                                <div class="help-block with-errors"></div>
                                             </div>
                                         </div>
 
@@ -108,7 +116,7 @@
                                                 <div class="col-xl-10 col-lg-9 col-sm-10">
                                                     <div class="form-check mb-2">
                                                         <div class="custom-control custom-radio classic-radio-info">
-                                                            <input type="radio" id="hRadio1" name="lecturer_gender" value="Male" class="custom-control-input">
+                                                            <input type="radio" id="hRadio1" name="lecturer_gender" value="Male" class="custom-control-input" checked>
                                                             <label class="custom-control-label" for="hRadio1">Male</label>
                                                         </div>
                                                     </div>
@@ -118,7 +126,6 @@
                                                             <label class="custom-control-label" for="hRadio2">Female</label>
                                                         </div>
                                                     </div>
-                                                    <label id="gender_error"></label>
                                                 </div>
                                             </div>
                                         </fieldset>
@@ -126,8 +133,8 @@
                                         <div class="form-group row mb-4">
                                             <label for="lecturer_bod" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Date of Birth</label>
                                             <div class="col-xl-3 col-lg-9 col-sm-3">
-                                                <input type="date" class="form-control-rounded form-control" name="lecturer_bod" id="lecturer_bod" placeholder="">
-                                                <lable id="dob_error"></lable>
+                                                <input type="date" class="form-control-rounded form-control" name="lecturer_bod" id="lecturer_bod" required>
+                                                <div class="help-block with-errors"></div>
                                             </div>
                                         </div>
 
@@ -136,16 +143,16 @@
                                         <div class="form-group row mb-4">
                                             <label for="lecturer_contact_no" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Contact Number</label>
                                             <div class="col-xl-10 col-lg-9 col-sm-10">
-                                                <input type="tel" class="form-control-rounded form-control" name="lecturer_contact_no" id="lecturer_contact_no" pattern="[0-9]{10}">
-                                                <lable id="phone_error"></lable>
+                                                <input type="tel" class="form-control-rounded form-control" name="lecturer_contact_no" id="lecturer_contact_no" minlength="10" maxlength="10" data-required-error="Please enter a valid 10-digit phone number" required >
+                                                <div class="help-block with-errors"></div>
                                             </div>
                                         </div>
 
                                         <div class="form-group row mb-4">
                                             <label for="lecturer_address" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Address</label>
                                             <div class="col-xl-10 col-lg-9 col-sm-10">
-                                                <input type="text" class="form-control-rounded form-control" name="lecturer_address" id="lecturer_address" placeholder="">
-                                                <label id="address_error"></label>
+                                                <input type="text" class="form-control-rounded form-control" name="lecturer_address" id="lecturer_address" data-required-error="Please enter a address" required >
+                                                <div class="help-block with-errors"></div>
                                             </div>
                                         </div>
 
@@ -157,7 +164,6 @@
                                             </div>
                                         </div>
                                     </form>
-                                <!-- < ?php echo form_close()?> -->
                             </div>
                         </div>
                     </div>
@@ -165,159 +171,5 @@
                 
             </div>
         </div>
-
-<script src="public/assets_1/assets/js/libs/jquery-3.1.1.min.js"></script>
-
-<script> 
-            function lecturerValidateForm(){
-
-                // Lecturer No validation
-                var lecturerNo = document.getElementById("lecturer_no").value;
-                if (lecturerNo === "") {
-                    document.getElementById("lecturer_no_error").innerHTML = "Please enter lecturer Emp-number.";
-                    document.getElementById("lecturer_no_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("lecturer_no_error").innerHTML = "";
-                }
-
-                // First Name validation
-                var firstName = document.getElementById("lecturer_first_name").value;
-                if (firstName === "") {
-                    // document.getElementById("first_name_error").empty();
-                    document.getElementById("first_name_error").innerHTML = "Please enter a first name.";
-                    document.getElementById("first_name_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("first_name_error").innerHTML = "";
-                }
-
-                // Last Name validation
-                var lastName = document.getElementById("lecturer_last_name").value;
-                if (lastName === "") {
-                    document.getElementById("last_name_error").innerHTML = "Please enter a last name.";
-                    document.getElementById("last_name_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("last_name_error").innerHTML = "";
-                }
-
-                // NIC validation
-                var nic = document.getElementById("lecturer_nic").value;
-                var nicRegex = /^[0-9]{9}[vVxX]$/;
-                if (nic === "" || !nicRegex.test(nic)) {
-                    document.getElementById("nic_error").innerHTML = "Please enter a valid NIC number.";
-                    document.getElementById("nic_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("nic_error").innerHTML = "";
-                }
-
-                // Email validation
-                var email = document.getElementById("lecturer_email").value;
-                var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                if (email === "" || !emailRegex.test(email)) {
-                    document.getElementById("email_error").innerHTML = "Please enter a valid email address.";
-                    document.getElementById("email_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("email_error").innerHTML = "";
-                }
-
-                // Gender validation
-                var gender = document.querySelector('input[name="lecturer_gender"]:checked');
-                if (!gender) {
-                    document.getElementById("gender_error").innerHTML = "Please select a gender.";
-                    document.getElementById("gender_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("gender_error").innerHTML = "";
-                }
-
-                // Date of Birth validation
-                var dob = document.getElementById("lecturer_bod").value;
-                if (dob === "") {
-                    document.getElementById("dob_error").innerHTML = "Please select a date of birth.";
-                    document.getElementById("dob_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("dob_error").innerHTML = "";
-                }
-
-
-                // Phone number validation
-                var phoneNumber = document.getElementById("lecturer_contact_no").value;
-                var phoneNumberRegex = /^[0-9]{10}$/;
-                if (phoneNumber === "" || !phoneNumberRegex.test(phoneNumber)) {
-                    document.getElementById("phone_error").innerHTML = "Please enter a valid 10-digit phone number.";
-                    document.getElementById("phone_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("phone_error").innerHTML = "";
-                }
-
-                // Address validation
-                var address = document.getElementById("lecturer_address").value;
-                if (address === "") {
-                    document.getElementById("address_error").innerHTML = "Please enter an address.";
-                    document.getElementById("address_error").style.color = "red";
-                    return false;
-                } else {
-                    document.getElementById("address_error").innerHTML = "";
-                }
-
-                // Rest of the validation code
-
-                return true;
-
-
-            }
-
-        </script>
-
-
-
-
-
-<!-- <script>
-$(document).ready(function(){
-
-    alert("test");
-
-
-
-
-$('#lectureform').submit(function(e){
-
-  // var isformvalid =  validateForm();
-
-  // reurn isformvalid;
-
-  var nic = $("#lecturer_nic").val();
-
-  if($('input[name="lecturer_gender"]:checked').length>0){
-    alert("gender selected");
-  }else{
-    alert("no gender selected");
-   return false;
-  }
-
-  if(nic === ''){
-    alert("NIC is null");
-    return false;
-  }
-
-  
-
- // alert("sadasd");
-
-  return true;
-
-});
-
-});
-</script> -->
-
-
        
 <?= $this->endSection() ?>
