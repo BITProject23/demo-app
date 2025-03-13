1f787a25549435b851ed61846e6afba86d7410da
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:38:08 +0530

    Update side_bar_student.php

--- app/Views/layouts/side_bar_student.php
+++ app/Views/layouts/side_bar_student.php
@@ -5,7 +5,7 @@
                         <a href="#dashboard" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
                             <div class="">
                                 <i class="flaticon-computer-6 ml-3"></i>
-                                <span onclick="(function(){ window.location.href = '<?=base_url()?>/'; return false; })();return false;">Dashboard</span>
+                                <span onclick="(function(){ window.location.href = '<?=base_url()?>/Student_Portal'; return false; })();return false;">Dashboard</span>
 
                             </div>
 
@@ -17,24 +17,24 @@
                         
 
                     <li class="menu">
-                        <a href="#elements" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                            <div class="">
-                                <i class="flaticon-layers"></i>
-                                <span>Batch Management</span>
-                            </div>
-                            <div>
-                                <i class="flaticon-right-arrow"></i>
-                            </div>
-                        </a>
-                        <ul class="collapse submenu list-unstyled" id="elements" data-parent="#accordionExample">
+                            <a href="#tables" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                                <div class="">
+                                    <i class="flaticon-square-menu"></i>
+                                    <span>Assignment</span>
+                                </div>
+                                <div>
+                                    <i class="flaticon-right-arrow"></i>
+                                </div>
+                            </a>
+                            <ul class="collapse submenu list-unstyled" id="tables" data-parent="#accordionExample">
+                                
+                                
+                                <li>
+                                    <a href="<?=base_url()?>/Get_Marks"> View Marks </a>
+                                </li> 
 
 
-                            <li>
-                                <a href="<?=base_url()?>/Batch_View"> View Batch</a>
-                            </li>
-                            
-                            
-                        </ul>
+                            </ul>
                     </li>
 
                     
@@ -53,49 +53,27 @@
                             
                             
                             <li>
-                                <a href="<?=base_url()?>/Payment_View"> View Payments </a>
+                                <a href="<?=base_url()?>/Student_Payment"> View Payments </a>
                             </li> 
 
 
                         </ul>
                     </li>
 
-                    <li class="menu">
-                        <a href="#charts" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                            <div class="">
-                                <i class="flaticon-credit-card-1"></i>
-                                <span>Assignment</span>
-                            </div>
-                            <div>
-                                <i class="flaticon-right-arrow"></i>
-                            </div>
-                        </a>
-                        <ul class="collapse submenu list-unstyled" id="charts" data-parent="#accordionExample">
-                            
-                            
-                            <li>
-                                <a href="<?=base_url()?>/Get_Marks"> View Marks </a>
-                            </li> 
-
-
-                        </ul>
-                    </li>
+                   
                     
 
                     <li class="menu">
                         <a href="#users" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
                                 <i class="flaticon-user-6"></i>
-                                <span> Profile <?= session()->get('user_name')?></span>
+                                <span> Profile -  <?= session()->get('user_name')?></span>
                             </div>
                             <div>
                                 <i class="flaticon-right-arrow"></i>
                             </div>
                         </a>
                         <ul class="collapse submenu list-unstyled" id="users" data-parent="#accordionExample">
-                            <li>
-                                <a href="user_profile.html"> Profile </a>
-                            </li>
                             <li>
                                 <a href="<?=base_url()?>/User_PasswordReset"> Change Password </a>
                             </li>
