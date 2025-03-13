d99b541d8103f36c3a230dc60471ff98442c4ad6
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:36:26 +0530

    Update EnrollmentModel.php

--- app/Models/EnrollmentModel.php
+++ app/Models/EnrollmentModel.php
@@ -10,6 +10,7 @@ class EnrollmentModel extends Model
     protected $table = 'tbl_enrollment';
     protected $primaryKey = 'en_id';
     protected $allowedFields =['en_date','student_id','course_id','batch_id'];
+    protected $useSoftDeletes = 'true';
 
 
 
