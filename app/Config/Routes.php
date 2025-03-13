8b358eb8668b0d4a7ca2bfe69e4575352dbf6efd
BITProject23 <129650423+BITProject23@users.noreply.github.com>
2025-03-14 04:35:47 +0530

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -54,24 +54,28 @@ $menu_items ['Dashboard_View'] = 'DashboardController::index';
 $menu_items ['Register'] = 'SigninController::index'; //ex : [url pattern] = 'Controller_name :: method(index)'
 $menu_items ['Regcreate'] = 'SigninController::create';
 
-if(session('user_type') == "ADMIN") {
+
 
 $menu_items ['User_add'] = 'UserController::index';
 $menu_items ['User_create'] = 'UserController::create';
 $menu_items ['User_View'] = 'UserController::showdata';
 $routes->get('User_Edit/(:num)', 'UserController::editdata/$1');
+$routes->post('User_Update', 'UserController::updatedata');
 $routes->get('User_Delete/(:num)', 'UserController::deleteData/$1');
 
-}
+
 
 $menu_items ['User_PasswordReset'] = 'LoginController::passwordReset';
 $menu_items ['User_PasswordResetAction'] = 'LoginController::passwordResetAction';
 
-
+if(session('user_type') == "ADMIN") {
 
 $menu_items ['Course_add'] = 'CourseController::index';
 $menu_items ['Course_create'] = 'CourseController::create';
 $menu_items ['Course_View'] = 'CourseController::showdata';
+$routes->get('Course_Edit/(:num)','CourseController::editdata/$1');
+$routes->post('Course_Update','CourseController::updatedata');
+$routes->get('Course_Delete/(:num)', 'CourseController::deleteData/$1');
 
 
 //$menu_items ['Course_UpdateView'] = 'CourseController::singleCourse';
@@ -83,11 +87,15 @@ $menu_items ['Subject_create'] = 'SubjectController::create';
 $menu_items ['Subject_View'] = 'SubjectController::showdata';
 $routes->get('Subject_Edit/(:num)', 'SubjectController::editdata/$1');
 $routes->post('Subject_Update', 'SubjectController::updatedata');
+$routes->get('Subject_Delete/(:num)', 'SubjectController::deleteData/$1');
 $menu_items ['Subject_By_Batch'] = 'SubjectController::subjectByBatch';
 
 $menu_items ['Subject_assign'] = 'CourseSubjectController::index';
-$menu_items ['Subject_save'] = 'CourseSubjectController::create';
-$menu_items ['Course_join'] = 'CourseSubjectController::courseSubjectJoin';
+$menu_items ['courseSubject_create'] = 'CourseSubjectController::create';
+$menu_items ['courseSubject_View'] = 'CourseSubjectController::courseSubjectview';
+$routes->get('courseSubjectEdit/(:num)', 'CourseSubjectController::edit/$1');
+$routes->post('courseSubjectUpdate', 'CourseSubjectController::update');
+$routes->get('courseSubject_Delete/(:num)', 'CourseSubjectController::deleteData/$1');
 
 
 $menu_items ['Lecturer_add'] = 'LecturerController::index';
@@ -95,14 +103,15 @@ $menu_items ['Lecturer_create'] = 'LecturerController::create';
 $menu_items ['Lecturer_View'] = 'LecturerController::showdata';
 $routes->get('Lecturer_Edit/(:num)', 'LecturerController::editdata/$1');
 $routes->post('Lecturer_Update', 'LecturerController::updatedata');
+$routes->get('Lecturer_Delete/(:num)', 'LecturerController::deleteData/$1');
 
 $menu_items ['Appointment_add/(:num)'] = 'AppointController::index/$1';
 $menu_items ['Appointment_create'] = 'AppointController::create';
 $menu_items ['Appointment_View'] = 'AppointController::showdata';
+$routes->get('Appointment_Delete/(:num)', 'AppointController::deleteData/$1');
 
 $menu_items ['Subject_By_Course'] = 'CourseSubjectController::subjectByCourse';
-$menu_items ['CourseLecturer_save'] = 'CourseLecturerController::save';
-$menu_items ['CourseLecturer_View'] = 'CourseLecturerController::courseLecturerJoin';
+
 
 $menu_items ['Batch_add'] = 'BatchController::index';
 $menu_items ['getCourseCode'] = 'BatchController::getCourseCode';
@@ -116,11 +125,15 @@ $routes->post('Batch_Timetable_create' , 'BatchTimeFrameController::create');
 
 $menu_items ['Batch_By_Course'] = 'BatchController::batchByCourse';
 
+$routes->post('load_subjectTime','BatchTimeFrameController::load_subject_time_data');
+
 $menu_items ['Batch_By_Enrollment'] = 'EnrollmentController::batchByEnrollment';
 $menu_items ['Enroll_Student/(:num)'] = 'EnrollmentController::index/$1';
 $menu_items ['Enrollment_create'] = 'EnrollmentController::create';
 $menu_items ['Enrollment_View'] = 'EnrollmentController::showdata';
+$routes->get('Enrollment_Delete/(:num)', 'EnrollmentController::deleteData/$1');
 
+}
 // $menu_items ['Enrollment_data_search'] = 'EnrollmentController::searchdata';
 
 
@@ -132,11 +145,14 @@ $routes->get('Student_Delete/(:num)', 'StudentContoller::deleteData/$1');
 $routes->post('Student_Update', 'StudentContoller::updatedata');
 $menu_items ['Student_By_Batch'] = 'StudentContoller::studentBybatch';
 
+$menu_items ['Students_payment'] = 'StudentContoller::paymentBystudents';
+
 
 $menu_items ['Payment_add'] = 'PaymentController::index';
 $menu_items ['Payment_create'] = 'PaymentController::create';
 $menu_items ['Payment_View'] = 'PaymentController::showdata';
 
+if(session('user_type') == "ADMIN") {
 
 $menu_items ['Payment_Search'] = 'ReportController::paymentsearch';
 $menu_items ['Payment_Search_View'] = 'ReportController::paymentview';
@@ -145,18 +161,28 @@ $menu_items ['Enrollment_Search_View'] = 'ReportController::enrollmentview';
 $menu_items ['Timetable_Search'] = 'ReportController::timetableSearch';
 $menu_items ['Timetable_Search_View'] = 'ReportController::timetableview';
 
+}
+
 $menu_items ['Logout'] = 'LogoutController::index';
 
 
 $menu_items ['Timetable'] = 'TimetableController::index';
 
+if(session('user_type') == "LECTURER") {
+
 $menu_items ['Lecturer_Portal'] = 'LecturerPortalController::index';
 $menu_items ['Assign_Marks'] = 'LecturerPortalController::create_marks';
 $menu_items ['Store_Marks'] = 'LecturerPortalController::store_marks';
 $menu_items ['View_Marks'] = 'LecturerPortalController::view_marks';
+$menu_items ['View_Timetable'] = 'LecturerPortalController::view_timetable';
+$menu_items ['View_Appointment'] = 'LecturerPortalController::showAppointData';
+$menu_items ['Subject_By_Batch'] = 'SubjectController::subjectByBatch';
+
+}
 
 $menu_items ['Student_Portal'] = 'StudentPortalController::index';
 $menu_items ['Get_Marks'] = 'StudentPortalController::get_marks';
+$menu_items ['Student_Payment'] = 'StudentPortalController::showdata';
 
 $menu_items ['Receptionist_Portal'] = 'ReceptionistPortalContoller::index';
 $menu_items ['View_Students'] = 'ReceptionistPortalContoller::view_students';
@@ -179,11 +205,7 @@ $menu_items ['test-db'] = 'DbTestController::index';
 
 
 // $routes->get('Course_UpdateView','CourseController::singleCourse');
-$routes->get('Course_Edit/(:num)','CourseController::editdata/$1');
-$routes->post('Course_Update','CourseController::updatedata');
 
-$routes->get('courseSubjectEdit/(:num)', 'CourseSubjectController::edit/$1');
-$routes->post('courseSubjectUpdate', 'CourseSubjectController::update');
 
 // $routes->get('load','ExampleController::index');
 // $routes->post('load_data','ExampleController::load_data');
